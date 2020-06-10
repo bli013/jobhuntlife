ad19a5e6beb0d3926494615c17854fe816d11fa2
bli013 <btli@ymail.com>
2020-06-10 10:15:23 -0700

    init commit

---
+++ jobhuntlife/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Seconds from './components/index'
+
+class App extends React.Component {
+  render(){
+
+  
+    return (
+      <>
+        <Seconds />
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ jobhuntlife/src/components/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./style.css";
+
+class Seconds extends React.Component {
+  
+  
+    render(){
+
+  
+        return (
+            <div>
+                <h1>Seconds component</h1>
+            </div>
+        )
+    }
+}
+
+export default Seconds;
